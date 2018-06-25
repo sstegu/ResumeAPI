541725d3da1185878179f46b4271ac7152d2afab
S. Stegu <sashastegu@gmail.com>
2018-06-25 11:25:29 -0400

    remove methods that aren't needed

--- ResumeAPI/Controllers/CVContentsController.cs
+++ ResumeAPI/Controllers/CVContentsController.cs
@@ -27,95 +27,95 @@ namespace ResumeAPI.Controllers
             return _context.CVContent.Where(x => x.Company.Guid == guid).Include(x => x.Company).Include(x => x.Company.Address);
         }
 
-        // GET: api/CVContents/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetCVContent([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// GET: api/CVContents/5
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetCVContent([FromRoute] int id)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var cVContent = await _context.CVContent.SingleOrDefaultAsync(m => m.ID == id);
+        //    var cVContent = await _context.CVContent.SingleOrDefaultAsync(m => m.ID == id);
 
-            if (cVContent == null)
-            {
-                return NotFound();
-            }
+        //    if (cVContent == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(cVContent);
-        }
+        //    return Ok(cVContent);
+        //}
 
-        // PUT: api/CVContents/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutCVContent([FromRoute] int id, [FromBody] CVContent cVContent)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// PUT: api/CVContents/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutCVContent([FromRoute] int id, [FromBody] CVContent cVContent)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != cVContent.ID)
-            {
-                return BadRequest();
-            }
+        //    if (id != cVContent.ID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            _context.Entry(cVContent).State = EntityState.Modified;
+        //    _context.Entry(cVContent).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CVContentExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!CVContentExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return NoContent();
-        }
+        //    return NoContent();
+        //}
 
-        // POST: api/CVContents
-        [HttpPost]
-        public async Task<IActionResult> PostCVContent([FromBody] CVContent cVContent)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// POST: api/CVContents
+        //[HttpPost]
+        //public async Task<IActionResult> PostCVContent([FromBody] CVContent cVContent)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            _context.CVContent.Add(cVContent);
-            await _context.SaveChangesAsync();
+        //    _context.CVContent.Add(cVContent);
+        //    await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCVContent", new { id = cVContent.ID }, cVContent);
-        }
+        //    return CreatedAtAction("GetCVContent", new { id = cVContent.ID }, cVContent);
+        //}
 
-        // DELETE: api/CVContents/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCVContent([FromRoute] int id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// DELETE: api/CVContents/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteCVContent([FromRoute] int id)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var cVContent = await _context.CVContent.SingleOrDefaultAsync(m => m.ID == id);
-            if (cVContent == null)
-            {
-                return NotFound();
-            }
+        //    var cVContent = await _context.CVContent.SingleOrDefaultAsync(m => m.ID == id);
+        //    if (cVContent == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            _context.CVContent.Remove(cVContent);
-            await _context.SaveChangesAsync();
+        //    _context.CVContent.Remove(cVContent);
+        //    await _context.SaveChangesAsync();
 
-            return Ok(cVContent);
-        }
+        //    return Ok(cVContent);
+        //}
 
         private bool CVContentExists(int id)
         {
